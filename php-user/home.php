96e17de77fca8bee2df8e9d3e1d6b1c3e600ee37
Randy <randyliminson@gmail.com>
2025-06-24 19:37:06 +0700

    Pengembangan Location

--- php-user/home.php
+++ php-user/home.php
@@ -17,11 +17,19 @@ $qTypes = $db->prepare($sqlTypes);
 $qTypes->execute();
 $listTypes = $qTypes->fetchAll(PDO::FETCH_ASSOC);
 
+
+$sqlShowrooms = "SELECT id, name, address, phone, opening_hours FROM showrooms WHERE is_active = 1 ORDER BY name ASC";
+$qShowrooms = $db->prepare($sqlShowrooms);
+$qShowrooms->execute();
+$listShowrooms = $qShowrooms->fetchAll(PDO::FETCH_ASSOC);
+
+
 $twig = Twig::make('../templates-user');
 echo $twig->render(
     'home.twig.html',
     [
       'brands'=> $listBrands,
-      'types'=> $listTypes
+      'types'=> $listTypes,
+      'showrooms'=> $listShowrooms
     ]
 );
