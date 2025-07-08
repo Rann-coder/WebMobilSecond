92b5f689dcfec79a88535d37f743cf3ab17eea1f
bryanncn <bryancen.11.2005@gmail.com>
2025-07-08 18:13:37 +0700

    fix database

--- database/seeds/0003_compare_cars_sample_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO compare_cars (name, brand, year, price, image_url, engine_cc, km, transmission, fuel_type, previous_owners)
-VALUES 
-('Avanza 1.5 G CVT', 'Toyota', 2024, 272500000, 'images/raize.png', 1500, 15000, 'Automatic', 'Gasoline', 1),
-('Brio Satya E CVT', 'Honda', 2025, 198300000, 'images/raize.png', 1200, 5500, 'CVT', 'Gasoline', 1),
-('Air EV Long Range', 'Wuling', 2024, 299500000, 'images/raize.png', 0, 10000, 'Automatic', 'Electric', 1);
